2ede9093f7c9f945f675c720026f34d87f2e40a8
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-06-05 18:05:29 +0900

    [ADD] 오다꾸 Backend 초기 설정파일 세팅

---
+++ odakku/src/main/java/com/kosa/odakku/OdakkuApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.odakku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdakkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdakkuApplication.class, args);
+	}
+
+}
---
+++ odakku/src/test/java/com/kosa/odakku/OdakkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.odakku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdakkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
