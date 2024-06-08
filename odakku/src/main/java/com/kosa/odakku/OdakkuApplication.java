44afa3dc10d659245314dd7242f18ce5302cc43c
LeeZEun <je990420@ajou.ac.kr>
2024-06-08 22:00:47 +0900

    [Test] 디스코드 깃헙 연동 테스트2

--- odakku/src/main/java/com/kosa/odakku/OdakkuApplication.java
+++ odakku/src/main/java/com/kosa/odakku/OdakkuApplication.java
@@ -6,7 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OdakkuApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OdakkuApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(OdakkuApplication.class, args);}
 }
